206b4d5108eca738a70616533099e7a4cc968b7f
habvi <a2231250601h@yahoo.co.jp>
2022-05-12 16:13:17 +0900

    change params

--- main.py
+++ main.py
@@ -1130,7 +1130,7 @@ def main():
     m = int(input())
     human_xy = [tuple(map(lambda x: int(x) - 1, input().split())) for _ in range(m)]
 
-    if n / m >= 2.6:
+    if n / m >= 2.7:
         g = Global_3(n, pets_xyk, m, human_xy)
         g.set_place()
         for _ in range(T):
